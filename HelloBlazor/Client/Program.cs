c10c6e2777b8ca89869069b964f7f0daa4ed0491
mike <mmunoz@msm-networking.com>
2022-11-22 13:38:14 -0600

    add Blazor WebAssembly app

---
+++ HelloBlazor/Client/Program.cs
@@ -0,0 +1,20 @@
+using HelloBlazor;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace HelloBlazor
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
--- HelloGitHub/Program.cs
+++ HelloGitHub/Program.cs
@@ -11,11 +11,11 @@ namespace HelloGitHub
 
             app.MapGet("/", (HttpResponse response) =>
             {
-                string Message = 
-                @"
+                string Message = @"
 <link href=""https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"" 
 rel=""stylesheet"" integrity=""sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"" 
 crossorigin=""anonymous"">
+
 <p class=""container alert alert-info mt-5"">
 Este código fue diseñado para ejemplificar el despliegue de <b><i>Azure App Service Web App</i></b> 
 desde un repositorio de GitHub.</p>
